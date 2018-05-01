b701b0c630c7e42d6755af6f595bb08a7d546abb
Tendai <tendai@ocastastudios.com>
2018-05-01 07:56:10 +0100

    Added tests for ScannedItem pricing

--- CheckoutTests/ScannedItemTests.swift
+++ CheckoutTests/ScannedItemTests.swift
@@ -27,4 +27,18 @@ class ScannedItemTests: XCTestCase {
         XCTAssertNil(uut, "Expected UUT to be nil given bad input")
     }
     
+    func testPrices() {
+        let appleString = "apple"
+        guard let uut = ScannedItem(rawValue: appleString) else {
+            return XCTFail("Failed to init ScannedItem of type .apple")
+        }
+        XCTAssert(uut.price == 0.6, "Expected ScannedItem of type .apple to have a price of 0.6")
+        
+        let orangeString = "orange"
+        guard let uut2 = ScannedItem(rawValue: orangeString) else {
+            return XCTFail("Failed to init ScannedItem of type .orange")
+        }
+        XCTAssert(uut2.price == 0.25, "Expected ScannedItem of type .orange to have a price of 0.25")
+    }
+    
 }
