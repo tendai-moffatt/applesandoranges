9ce385c0cbb1701d83a0c168e08f7a8a32533e3f
Tendai <tendai@ocastastudios.com>
2018-05-01 09:13:11 +0100

    Added total price calculation to ShoppingCart

--- Checkout/Model/ShoppingCart.swift
+++ Checkout/Model/ShoppingCart.swift
@@ -11,15 +11,32 @@ import Foundation
 class ShoppingCart: NSObject {
     
     var items: [ScannedItem]
+    var discounts: [DiscountOffer]
     var subtotal: Double {
         return items.reduce(0, { return $0 + $1.price })
+    }
+    var total: Double {
+        var cartTotal = subtotal
+        for discount in discounts {
+            let qualifyingItems = items.filter({ $0 == discount.qualifyingItemType })
+            let matchCount = qualifyingItems.count - (qualifyingItems.count % discount.minimumItemCount)
+            let discount = Double(matchCount) * discount.qualifyingItemType.price * discount.priceMultiplier
+            cartTotal -= discount
+        }
+        return cartTotal
     }
     
     override init() {
         self.items = [ScannedItem]()
+        self.discounts = [DiscountOffer]()
     }
     
-    init(items: [ScannedItem]) {
-        self.items = items
+    init(items: [ScannedItem]? = nil, discounts: [DiscountOffer]? = nil) {
+        self.items = items ?? [ScannedItem]()
+        self.discounts = discounts ?? [DiscountOffer]()
+    }
+    
+    func apply(offer: DiscountOffer) {
+        discounts.append(offer)
     }
 }
